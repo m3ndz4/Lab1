003e2591ccc3660fe36eb4e72926bdd2b558abae
juanq <juanq@DESKTOP-IGRKQMB>
2018-03-06 23:40:14 -0500

    Add project files.

---
+++ Lab1/Lab1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLab1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUnitInput = New System.Windows.Forms.Label()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtAllUnits = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblUnitInput
+        '
+        Me.lblUnitInput.AutoSize = True
+        Me.lblUnitInput.Location = New System.Drawing.Point(18, 20)
+        Me.lblUnitInput.Name = "lblUnitInput"
+        Me.lblUnitInput.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnitInput.TabIndex = 0
+        Me.lblUnitInput.Text = "Units:"
+        '
+        'lblDays
+        '
+        Me.lblDays.AutoSize = True
+        Me.lblDays.Location = New System.Drawing.Point(301, 20)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(49, 17)
+        Me.lblDays.TabIndex = 1
+        Me.lblDays.Text = "Day: 1"
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(18, 199)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(332, 23)
+        Me.lblResult.TabIndex = 2
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(68, 17)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(227, 22)
+        Me.txtInput.TabIndex = 1
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(17, 225)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(75, 34)
+        Me.btnEnter.TabIndex = 3
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(98, 226)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 33)
+        Me.btnReset.TabIndex = 4
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(179, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 33)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtAllUnits
+        '
+        Me.txtAllUnits.Location = New System.Drawing.Point(17, 45)
+        Me.txtAllUnits.Multiline = True
+        Me.txtAllUnits.Name = "txtAllUnits"
+        Me.txtAllUnits.ReadOnly = True
+        Me.txtAllUnits.Size = New System.Drawing.Size(333, 151)
+        Me.txtAllUnits.TabIndex = 6
+        '
+        'frmLab1
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(371, 271)
+        Me.Controls.Add(Me.txtAllUnits)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblDays)
+        Me.Controls.Add(Me.lblUnitInput)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmLab1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUnitInput As Label
+    Friend WithEvents lblDays As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtAllUnits As TextBox
+End Class
---
+++ Lab1/Lab1.vb
@@ -0,0 +1,112 @@
+﻿Option Strict On
+Public Class frmLab1
+    'ARRAY/STATEMENTS
+    Dim arrayUnits(6) As Integer
+    Dim indexIncrement As Integer
+    Dim totUnits As Integer
+    Dim avgUnits As Double
+    Const maxIncrement As Integer = 7
+    Const minValue As Integer = 0
+    Const maxValue As Integer = 1000
+
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Ends application
+        Application.Exit()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'STATEMENTS
+        Dim inputInt As Integer
+
+        'Checks if there is input within the textbox
+        If (Len(txtInput.Text) = 0) Then
+            btnEnter.Enabled = False
+            'Disables Enter button if textbox is empty
+        Else
+            'Checks if input is an integer
+            If (Integer.TryParse(txtInput.Text, inputInt)) Then
+
+                'Checks if imput is  within range
+                If (inputInt <= maxValue And inputInt >= minValue) = True Then
+
+                    lblResult.ForeColor = Color.Black 'Changes Results label to black forecolour post error
+                    lblResult.Text = ""
+
+                    'Converts input into integer and stores it into the array
+                    arrayUnits(indexIncrement) = Convert.ToInt32(txtInput.Text)
+                    txtAllUnits.Text += arrayUnits(indexIncrement) & vbCrLf
+                    indexIncrement += 1 'Incrementer
+
+                    'Initiates calculation once 7 days of units have been filled in
+                    If (indexIncrement = maxIncrement) Then
+                        btnEnter.Enabled = False
+                        For indexIncrement = 0 To maxIncrement - 1
+                            totUnits += arrayUnits(indexIncrement)
+                            'Totals all units
+                        Next
+                        avgUnits = totUnits / (maxIncrement)
+                        'Calculates Average Units
+                        lblResult.Text = "Average per day: " & avgUnits.ToString("n2") '.ToString("n2") rounds to 2 decimal places
+                    End If
+                    txtInput.Text = ""
+                    lblDays.Text = "Day: " & indexIncrement + 1
+                    'Day incrementer
+
+                    If (indexIncrement = maxIncrement) Then
+                        lblDays.Text = "Day: " 'Sets Days to none during calculation
+                        txtInput.Enabled = False
+                        'Disables Enter once all 7 days have been entered
+                    End If
+                Else
+                    'Produces Error message for bad range
+                    lblResult.ForeColor = Color.Red
+                    lblResult.Text = "Bad input, please input a value between 0 and 1000"
+                    txtInput.Text = ""
+                End If
+            Else
+                'Produces Error message for bad inputs
+                lblResult.ForeColor = Color.Red
+                lblResult.Text = "Bad input, please input an Integer"
+                txtInput.Text = ""
+            End If
+        End If
+
+    End Sub
+
+    Private Sub txtAllUnits_TextChanged(sender As Object, e As EventArgs) Handles txtAllUnits.TextChanged
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Resets the array
+        For incrementReset As Integer = 0 To maxIncrement - 1
+            arrayUnits(incrementReset) = 0
+        Next
+
+        'Clears/Resets all text
+        txtInput.Text = ""
+        txtAllUnits.Text = ""
+        lblResult.Text = ""
+        lblDays.Text = "Day: 1"
+
+        'Enables specific controls post disabling
+        btnEnter.Enabled = True
+        txtInput.Enabled = True
+
+        'Resets variables to 0
+        indexIncrement = 0
+        totUnits = 0
+        avgUnits = 0
+
+        'Shifts focus back to the input control
+        txtInput.Focus()
+    End Sub
+
+    Private Sub lblDays_Click(sender As Object, e As EventArgs) Handles lblDays.Click
+
+    End Sub
+End Class
